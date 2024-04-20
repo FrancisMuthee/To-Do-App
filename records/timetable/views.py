f196547ff7ab8af0a35d4104d90a6d1bc3262603
Francis <francisnjaramba2@gmail.com>
2024-04-20 13:54:40 +0300

    Successful addings

--- records/timetable/views.py
+++ records/timetable/views.py
@@ -4,16 +4,17 @@ from .models import Task
 
 def home(request):
     return render(request, 'index.html')
+    
 
 def task_list(request):
-    task= Task.objects.all()
-    return render(request, 'templates/index.html', {'tasks': task})
+    tasks= Task.objects.all()
+    return render(request, 'index.html', {'tasks': tasks})
 
 def add_task(request):
     if request.method == 'POST':
         title = request.POST['title']
-        Task.object.create(title=title)
-        return redirect(task_list)
+        Task.objects.create(title=title)
+        return redirect('task_list')
     
 def delete(request, task_id):
     task= Task.objects.get(pk=task_id) 
